315cb385081e1876a022e807ca818672a59c7a62
steve ansah ababio <stevo12424@live.com>
2022-12-07 00:56:08 +0000

    necessary change

--- src/utils/fetchweatherreport.ts
+++
@@ -1,7 +0,0 @@
-import fetch from "node-fetch";
-export async function fetchWeatherReport(url:string){
-    const jsonres = await fetch(url,{method:"GET"});
-    const response = await jsonres.json();
-    console.log(response);
-    return response;
-}
