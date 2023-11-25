dbc331f7ff3ccf795fbf1a10e2cb422dbd97f830
steve-ababio <stevo12424@live.com>
2023-11-25 04:48:07 +0000

    modified app.js

--- dist/data/config/database.js
+++ dist/data/config/database.js
@@ -4,11 +4,11 @@ let pool = null;
 
 exports.createDatabaseConnection = async function createDatabaseConnection() {
     pool = new Pool({
-        user: process.env.PG_USER,
-        host: process.env.PG_HOST,
-        database: process.env.PG_DATABASE,
-        password: process.env.PG_PASSWORD,
-        port: parseInt(process.env.PG_PORT),
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_DATABASE,
+        password: process.env.POSTGRES_PASSWORD,
+        port: parseInt(process.env.POSTGRES_PORT),
     });
     try {
         await pool.connect();
